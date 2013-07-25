72061e9e57e78e4cf0af49bcdbcfb1917316e0a3
GrahamA2 <graham.allan@bcs.org.uk>
2013-07-25 21:22:52 +0100

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,20 +1,14 @@
-package com.wakaleo.gameoflife.domain;
-
 public enum Cell {
-    LIVE_CELL("+"), DEAD_CELL(".");
-
+    LIVE_CELL("*"), DEAD_CELL(".");
     private String symbol;
-
-    private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
+    private Cell(String symbol) {
+        this.symbol = symbol;
     }
-
     @Override
     public String toString() {
         return symbol;
     }
-
-    static Cell fromSymbol(final String symbol) {
+    static Cell fromSymbol(String symbol) {
         Cell cellRepresentedBySymbol = null;
         for (Cell cell : Cell.values()) {
             if (cell.symbol.equals(symbol)) {
@@ -24,8 +18,7 @@ public enum Cell {
         }
         return cellRepresentedBySymbol;
     }
-
-    public String getSymbol() {
+        public String getSymbol() {
         return symbol;
     }
 }
